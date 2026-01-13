fe5dffd6f3a592c22526204d3b7cbbd7f7414517
Nireeksha Shetty <nireeksha@connectia.in>
2026-01-13 11:24:27 +0530

    minor changes

--- src/routes/testimonialRoutes.ts
+++ src/routes/testimonialRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import {createTestimonial, getTestimonials} from '../controllers/testimonial';
+import { createTestimonial, getTestimonials } from "../controllers/testimonial";
 import { authenticateJWT } from "../middlewares/auth";
 import { requireAdmin } from "../middlewares/adminAuth";
 
 const router = Router();
 router.post("/", authenticateJWT, requireAdmin, createTestimonial);
 router.get("/", getTestimonials);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,27 @@
 import { connectDB } from "./config/mongodb";
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import productRoutes from "./routes/productRoutes";
 import authRoutes from "./routes/authRoutes";
 import crewRoutes from "./routes/crewRoutes";
-import adminRoutes from "./routes/adminRoutes"; // 👈 Add this
+import adminRoutes from "./routes/adminRoutes";
+import testimonialRoutes from "./routes/testimonialRoutes"
 
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+  origin: ["http://localhost:5173/","https://naughtyjars.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/crew", crewRoutes);
-app.use("/api/admin", adminRoutes); // 👈 Add this
+app.use("/api/admin", adminRoutes);
+app.use("/api/testimonials", testimonialRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
