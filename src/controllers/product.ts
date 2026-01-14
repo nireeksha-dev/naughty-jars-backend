f6956e13b03f4191cfd171ecf54814e5a2f30ef3
Nireeksha Shetty <nireeksha@connectia.in>
2026-01-14 16:09:57 +0530

    testimonial added

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -4,16 +4,19 @@ import { Request, Response } from "express";
 export const addProduct = async (req: Request, res: Response) => {
   try {
     const { name, description, price } = req.body;
-    const images = req.body.images || []; 
+    const images = req.body.images || [];
     const product = await Product.create({ name, description, price, images });
     res.status(201).json(product);
+    if (!name || !price || !description) {
+      res.status(404).json({ success: false, message: "all fields required" });
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
-  try {
+  try { 
     const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
@@ -42,7 +45,7 @@ export const getProductDetails = async (req: Request, res: Response) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+};
 
 export const getAllProducts = async (req: Request, res: Response) => {
   try {
@@ -51,4 +54,4 @@ export const getAllProducts = async (req: Request, res: Response) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+};
